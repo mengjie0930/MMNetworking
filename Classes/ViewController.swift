c60e95e0e2fc17b3bf68255769087b1f5e2bace8
孟杰 <3307932850@qq.com>
2022-07-21 14:39:52 +0800

    测试

---
+++ Classes/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MMNetworking
+//
+//  Created by 孟杰 on 07/21/2022.
+//  Copyright (c) 2022 孟杰. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
